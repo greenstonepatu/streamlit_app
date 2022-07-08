5c12535dfa20243720d1cb04e7dc98accb5350cb
greenstonepatu <patrykgrabowski@gmail.com>
2022-07-08 13:19:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ streamlit.text('🥗Jabłka, Banany, Gruszki')
 streamlit.header('🥭Zbuduj swój własny owocowy koktajl')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = myfruit_list_set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Wybierz owoce:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
