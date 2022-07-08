f37b8a05f4980963cd879615f91ab98101fb5f2f
greenstonepatu <patrykgrabowski@gmail.com>
2022-07-08 13:13:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+import streamlit
+import pandas
+import snowflake.connector
+import request
+from urllib.error import URLError
 
+streamlit.title('Zdrowe Śniadanie')
+streamlit.text('Omega3 i Owśianka z jagodami')
+streamlit.text('Jabłka, Banany, Gruszki')
+streamlit.header('Zbuduj swój własny owocowy koktajl')
