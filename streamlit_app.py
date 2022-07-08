fb1d86ba29fe1ffe33d18caba0d5ed9405b92fbe
greenstonepatu <patrykgrabowski@gmail.com>
2022-07-08 13:38:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ def get_fruit_load_list():
   
   
 if streamlit.button('Wyświetl Liste Owoców'):
-  my_cnx = snowflake.connector.connect(**streamlit_secrets["snowflake"])
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
   streamlit.dataframe(my_data_rows)
